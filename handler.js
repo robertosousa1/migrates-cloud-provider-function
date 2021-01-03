b0e8f6f5daef15a7a0b1da4b3cd22842985abc84
Roberto <rm77831@fiap.com.br>
2021-01-03 20:56:12 -0300

    gera commit aleatório

--- handler.js
+++ handler.js
@@ -1,11 +1,18 @@
 'use strict';
 
-module.exports.hello = async event => {
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+module.exports.randomCommit = async event => {
+  const { data } = await axios.get('http://whatthecommit.com/')
+  const $ = cheerio.load(data)
+  const [commitMessage] = await $('#content').text().trim().split('\n')
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Hello, World!'
+        message: commitMessage
       }
     ),
   };
